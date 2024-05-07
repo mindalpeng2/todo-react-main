554a57226d3784c754aa4269ee425854b6a92ad9
mindalpeng2 <163081924+mindalpeng2@users.noreply.github.com>
2024-05-07 23:16:43 +0900

    kakao
    오류해결- route 수정, redirect url 수정

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -2,11 +2,12 @@ import NextAuth from "next-auth";
 import KakaoProvider from "next-auth/providers/kakao";
 
 const handler = NextAuth({
-    providers: [
-        KakaoProvider({
-            clientid: process.env.KAKAO_CLIENT_ID || "", 
-            clientSecret: process.env.KAKAO_CLIENT_SECRET || "",
-        }),
-    ],
-}) ;
+  providers: [
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID || "",
+      clientSecret: process.env.KAKAO_CLIENT_SECRET || "",
+    }),
+  ],
+});
+
 export { handler as GET, handler as POST };
